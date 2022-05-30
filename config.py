e6e87f29a95db842460c085854cea9f1e7bc87ba
Dmitrii Altukhov <dm.altukhov@ya.ru>
2022-05-30 17:53:24 +0300

    Remove old comment

--- config.py
+++ config.py
@@ -6,6 +6,5 @@ src_dir = root / ".." / "sourcedata"
 
 n_subj = len(list(src_dir.glob("sub-*")))
 all_subjects = sorted(str(i + 1).zfill(2) for i in range(n_subj))
-# all_subjects = ["26"]
 bad_subjects = ["41"]  # weird MRI: very few slices in each of 3 sequences
 subjects = [s for s in all_subjects if s not in bad_subjects]
